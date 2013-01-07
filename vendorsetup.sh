0e21e6d3e78106a87b8e349d9ed6604772bbfedf
Dorian Snyder <dastin1015@gmail.com>
2013-01-06 21:12:36 -0800

    shooteru: correct vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_shooteru-userdebug
-add_lunch_combo htc_shooteru-eng
+add_lunch_combo cm_shooteru-userdebug
