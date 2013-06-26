1199a9c1afe9109460d2cb7404fc51198e415936
Darescu Ionut <johnny@esi.ro>
2013-06-26 17:04:23 +0300

    update makefiles

--- liquid.mk
+++ liquid.mk
@@ -16,10 +16,11 @@ PRODUCT_NAME := liquid_shooteru
 PRODUCT_BRAND := HTC
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := Evo 3D GSM
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.shooteru.UNOFFICIAL.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
-# Bootanimation
-TARGET_SCREEN_HEIGHT := 960
+# Boot animation
 TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
 
 # Release name
 PRODUCT_RELEASE_NAME := shooteru
