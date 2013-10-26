56c9f89dd8af3c0d5629500d3dade83284406f7e
Darescu Ionut <ionut.darescu@gmail.com>
2013-10-26 14:22:02 +0300

    shooteru: Use a2dp tuning
    Change-Id: Ia7ef88d8fba88a7bc998dc8f10b4ddadb6351489

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,4 +20,8 @@
 #define BTM_DEF_LOCAL_NAME   "HTC Evo 3D"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
+#define A2DP_HW_SYSFS_TUNER "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+#define A2DP_HW_SYSFS_TUNER_OFF "0"
+#define A2DP_HW_SYSFS_TUNER_ON "432000"
+
 #endif
