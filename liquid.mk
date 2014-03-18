01fb874f50065faeacbd640bb9003f6639ad48e5
Dărescu Ionuț <ionut.darescu@gmail.com>
2014-03-18 10:19:51 +0200

    Update liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -4,11 +4,11 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 # Inherit some common Liquid stuff.
 $(call inherit-product, vendor/liquid/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/htc/shooteru/full_shooteru.mk)
-
 # Device naming
 PRODUCT_NAME := liquid_shooteru
+
+# Inherit device configuration
+$(call inherit-product, device/htc/shooteru/full_shooteru.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
